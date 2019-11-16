0b4db5c7bea87a62cb3dfde37dce8361f8baab5b
LIAN Wei Zheng <wzlian.2014@economics.smu.edu.sg>
2019-11-16 12:27:26 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("up and running!")	
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User filled "+ cardOne);
+console.log("User filled "+ cardThree);
