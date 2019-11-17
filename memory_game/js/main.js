8def50f25d5a028b644ed6394a7a0295178a0a52
LIAN Wei Zheng <wzlian.2014@economics.smu.edu.sg>
2019-11-17 13:18:00 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,41 @@
 console.log("up and running!")	
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User filled "+ cardOne);
-console.log("User filled "+ cardThree);
+
+
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+cardsInPlay.push(cardTwo);
+
+
+
+
+
+console.log(cardsInPlay);
+console.log("User flipped "+ cardTwo)
+
+if (cardsInPlay.length === 2) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) { 
+		alert("you found a match!")
+	}	
+
+	else {
+		alert("sorry,try again.")
+	}
+
+
+}
+
+else {
+	alert("please pick two cards.")
+
+}
+
+
